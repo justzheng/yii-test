0530cd0b7b82fbe590a6135f40644a3b4b9ba90a
zhengtaoyu <717828758@qq.com>
2019-01-15 12:53:54 +0800

    1

--- console/Job/Send.php
+++ console/Job/Send.php
@@ -20,6 +20,7 @@ class Send extends JobHandler
      */
     public function handle($job, $data)
     {
+        file_put_contents('/var/www/html/yii-test.log','12313',FILE_APPEND);
         Yii::warning('error','rpcerror');
         // TODO: Implement handle() method.
     }
