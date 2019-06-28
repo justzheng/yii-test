0b669f46d2052398b93e187645f66300674bc3b7
zhengtaoyu <717828758@qq.com>
2019-06-28 15:45:09 +0800

    1

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -24,38 +24,38 @@ class TestController extends \yii\console\Controller
         $limit = 1000;
 
         while (true) {
-            if(time()>1561706000){
-                echo "123";
-            }else{
-                sleep(1);
-            }
-//            $sql = "SELECT * FROM " . TableTitleRelation::tableName() . " WHERE status = :status ORDER BY id ASC LIMIT $offset, $limit";
-//
-//            $offset += $limit;
-//
-//            $data_reader = Yii::$app->db->createCommand($sql)->bindValues([
-//                ':status' => 1,
-//            ])->query();
-
-//            if ($data_reader->count() > 0) {
-//                while ($title = $data_reader->read()) {
-//                    if($title['expired_at'<time()]){
-//                        Yii::$app->db->createCommand("UPDATE " . TableTitleRelation::tableName() . " SET status=:status WHERE id=:id")->bindValues([
-//                            ':status' => 2,
-//                            ':id' => $title['id'],
-//                        ])->execute();
-////                        \Yii::$app->memberRemind->sendVerificationMessage($title['uid'], [
-////                            'title' => '称号变更',
-////                            'content' => '您的称号已到期'
-////                        ]);
-//                    }
-//                }
-//                sleep(10);
-//            } else {
-//                sleep(86400);
-//                $offset = 0;
+//            if(time()>1561706000){
+//                echo "123";
+//            }else{
+//                sleep(1);
 //            }
-            sleep(10);
+            $sql = "SELECT * FROM " . TableTitleRelation::tableName() . " WHERE status = :status ORDER BY id ASC LIMIT $offset, $limit";
+
+            $offset += $limit;
+
+            $data_reader = Yii::$app->db->createCommand($sql)->bindValues([
+                ':status' => 1,
+            ])->query();
+
+            if ($data_reader->count() > 0) {
+                while ($title = $data_reader->read()) {
+                    if($title['expired_at'<time()]){
+                        Yii::$app->db->createCommand("UPDATE " . TableTitleRelation::tableName() . " SET status=:status WHERE id=:id")->bindValues([
+                            ':status' => 2,
+                            ':id' => $title['id'],
+                        ])->execute();
+//                        \Yii::$app->memberRemind->sendVerificationMessage($title['uid'], [
+//                            'title' => '称号变更',
+//                            'content' => '您的称号已到期'
+//                        ]);
+                    }
+                }
+                sleep(10);
+            } else {
+                sleep(86400);
+                $offset = 0;
+            }
+//            sleep(10);
         }
     }
 }
