dc21ffe7c63d2fe4d84e54c6ea20d545a8d1ba81
zhengtaoyu <717828758@qq.com>
2019-08-13 15:38:13 +0800

    1

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -264,7 +264,7 @@ class SiteController extends Controller
     }
 
     public function actionDaw(){
-        $client = Client::create('http://127.0.0.1/yii-test/frontend/web/index.php/site/user/',false);
+        $client = Client::create('http://47.96.64.84/yii-test/frontend/web/index.php/site/user/',false);
         $res = $client->testSum(111);
         echo $res;
     }
@@ -275,7 +275,7 @@ class SiteController extends Controller
 
     public function actionWsdl(){
         $client = new \mongosoft\soapclient\Client([
-            'url' => 'http://127.0.0.1/yii-test/frontend/web/index.php/xml/wsdl',
+            'url' => 'http://47.96.64.84/yii-test/frontend/web/index.php/xml/wsdl',
             'options' => [
                 'cache_wsdl' => WSDL_CACHE_NONE,
             ]
